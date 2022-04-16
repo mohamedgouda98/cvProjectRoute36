a9cae43f88e5ca1bd45df6f4ef2563706ef15396
mohamed.sakr <mohamed.sakr@dimlocal.net>
2022-04-16 22:14:23 +0200

    portfolio and cat changes

---
+++ resources/views/Admin/portfolioCategory/index.blade.php
@@ -0,0 +1,83 @@
+@include('Admin.assets.header')
+
+        <!--  BEGIN CONTENT AREA  -->
+        <div id="content" class="main-content">
+            <div class="container">
+                <div class="container">
+
+                    <div class="row layout-top-spacing">
+                        <div id="tableDropdown" class="col-lg-12 col-12 layout-spacing">
+                            <div class="statbox widget box box-shadow">
+                                <div class="widget-header">
+                                    <div class="row">
+                                        <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                                            <h4>Portfolio Categories</h4>
+                                            <a href="{{route('admin.portfolio.category.create')}}" class="btn btn-primary"> Add New One</a>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                @if($errors->any())
+                                    @foreach($errors->all() as $error)
+                                        <div class="alert alert-danger" role="alert">
+                                            {{$error}}
+                                        </div>
+                                    @endforeach
+                                @endif
+
+                                @if(\Illuminate\Support\Facades\Session::has('done'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{\Illuminate\Support\Facades\Session::get('done')}}
+                                    </div>
+                                @endif
+                                <div class="widget-content widget-content-area">
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered mb-4">
+                                            <thead>
+                                                <tr>
+                                                    <th>Name</th>
+                                                    <th class="text-center">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            @foreach($portfolioCategories as $portfolioCategory)
+                                                <tr>
+                                                    <td>{{$portfolioCategory->name}}</td>
+                                                    <td class="text-center">
+
+                                                        <div class="dropdown custom-dropdown">
+                                                            <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
+                                                            </a>
+
+                                                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink1">
+                                                                <form method="post" action="{{route('admin.portfolio.category.delete')}}">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                    <input type="hidden" name="id" value="{{$portfolioCategory->id}}">
+                                                                    <button class="dropdown-item" type="submit">Delete</button>
+                                                                </form>
+                                                                <a class="dropdown-item" href="{{route('admin.portfolio.category.edit', [$portfolioCategory->id])}}">Edit</a>
+                                                            </div>
+                                                        </div>
+
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!--  END CONTENT AREA  -->
+    </div>
+
+@include('Admin.assets.footer')
